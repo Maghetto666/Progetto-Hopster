6737c7265eb2a301f2b91eda3ff6cbcdafe3ec8c
Maghetto666 <lordrey3@gmail.com>
2024-07-27 15:41:02 +0200

    first commit

---
+++ Sito/dbapi/src/test/java/it/hopster/dbapi/DbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.hopster.dbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
