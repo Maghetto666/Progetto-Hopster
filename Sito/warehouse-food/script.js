16d394502a4582ba94a6e30b657003989a3d5e4e
Maghetto666 <lordrey3@gmail.com>
2024-08-09 15:21:54 +0200

    fixata add-btn con campi required

--- Sito/warehouse-food/script.js
+++ Sito/warehouse-food/script.js
@@ -32,16 +32,39 @@ frozenCheckbox.addEventListener('click', function () {
     }
 });
 
-add_btn.addEventListener('click', function (){
-    const template = buildTemplateHTML();
-    itemsList.innerHTML += template;
+
+
+add_btn.addEventListener('click', function () {
+    let product = product_input.value.trim();
+    let quantity = quantity_input.value;
+    let deliveryDate = deliveryDate_input.value;
+    let expiringDate = expiringDate_input.value;
+    let frozenDate = frozenDate_input.value;
+
+    if (frozenCheckbox.checked == true) {
+        frozenDate_input.required = true;
+        if (product.length == 0 || quantity == 0 || deliveryDate == 0 || expiringDate == 0 || frozenDate == 0) {
+            return;
+        } else {
+            const template = buildTemplateHTML(product, quantity, deliveryDate, expiringDate, frozenDate);
+            itemsList.innerHTML += template;
+        }
+    } else {
+        frozenDate_input.required = false;
+        if (product.length == 0 || quantity == 0 || deliveryDate == 0 || expiringDate == 0) {
+            return;
+        } else {
+            const template = buildTemplateHTML(product, quantity, deliveryDate, expiringDate, frozenDate);
+            itemsList.innerHTML += template;
+        }
+    }
 });
 
 // Recupera dai dal local storage
 const STORAGE_KEY = 'foods-key';
 let products = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 
-// Mostra il contenuto della lista todo
+// Mostra il contenuto della lista
 showContent();
 
 // Funzione che aggiorna il contenuto della lista
@@ -58,35 +81,36 @@ function showContent() {
     }
 }
 
-// Funzione che genera l'HTML per attività todo
-function buildTemplateHTML(newProduct) {
+// Funzione che genera l'HTML per attività
+function buildTemplateHTML(typedProduct, typedQuantity, typedDeliveryDate, typedExpiringDate, typedFrozenDate) {
     if (frozenCheckbox.checked == true) {
+        frozenDate_input.required = true;
         return `
     <ul class="newItem">
                     <li class="checkbox-item"><input type="checkbox"></li>
-                    <li class="product-item">{typedProduct}</li>
-                    <li class="quantity-item">{typedQuantity}</li>
-                    <li class="deliveryDate-item">{typedDeliveryDate}</li>
-                    <li class="expiringDate-item">{typedExpiringDate}</li>
+                    <li class="product-item">${typedProduct}</li>
+                    <li class="quantity-item">${typedQuantity}</li>
+                    <li class="deliveryDate-item">${typedDeliveryDate}</li>
+                    <li class="expiringDate-item">${typedExpiringDate}</li>
                     <li class="frozen-item"><input type="checkbox" checked disabled></li>
-                    <li class="frozenDate-item">{typedFrozenDate}</li>
+                    <li class="frozenDate-item">${typedFrozenDate}</li>
                     <li class="modify-item"><button class="modify-btn">Modifica</button></li>
                 </ul>
     `
     } else {
-    return `
+        return `
     <ul class="newItem">
                     <li class="checkbox-item"><input type="checkbox"></li>
-                    <li class="product-item">{typedProduct}</li>
-                    <li class="quantity-item">{typedQuantity}</li>
-                    <li class="deliveryDate-item">{typedDeliveryDate}</li>
-                    <li class="expiringDate-item">{typedExpiringDate}</li>
+                    <li class="product-item">${typedProduct}</li>
+                    <li class="quantity-item">${typedQuantity}</li>
+                    <li class="deliveryDate-item">${typedDeliveryDate}</li>
+                    <li class="expiringDate-item">${typedExpiringDate}</li>
                     <li class="frozen-item"><input type="checkbox" disabled></li>
                     <li class="frozenDate-item" disabled></li>
                     <li class="modify-item"><button class="modify-btn">Modifica</button></li>
                 </ul>
     `
-}
+    }
 }
 
 warehouse_btn.addEventListener('mouseover', function () {
